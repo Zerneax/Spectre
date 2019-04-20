cde85a5009a755e65d3ee70a8b42eda7c4d37fbe
Chris <d.chris59@gmail.com>
2019-04-20 22:20:28 +0200

    initialize layout page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LayoutComponent } from './pages/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SummaryComponent } from './pages/summary/summary.component';
+import { LayoutComponent } from './pages/layout/layout.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
