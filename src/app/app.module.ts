4a183eedbd93248addd0dae66c971d28be92a410
Chris <d.chris59@gmail.com>
2019-04-20 22:37:30 +0200

    add back button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SummaryComponent } from './pages/summary/summary.component';
-import { NavigationComponent } from './pages/navigation/navigation.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
 
 @NgModule({
   declarations: [
