581edf1aca5c5090c797c803615d80739dc7e916
Chris <d.chris59@gmail.com>
2019-04-21 23:07:53 +0200

    oups i forgot this file

--- src/app/component/navigation/navigation.component.ts
+++ src/app/component/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { SidebarService } from 'src/app/services/sidebar/sidebar.service';
 
 @Component({
   selector: 'app-navigation',
@@ -8,9 +10,18 @@ import { Router } from '@angular/router';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private sidebarService: SidebarService) { }
 
   ngOnInit() {
+
   }
 
+  test() {
+    window.scrollTo(0,0);
+  }
+
+  openSidebar() {
+    this.sidebarService.openSidebar();
+  }
 }
